d1b1722b79b8cf0b6cdbeb3a229c8641ce57105f
rayalva407 <rayalva407@gmail.com>
2020-11-26 12:59:08 -0700

    generated User model

--- should-i-backend/config/application.rb
+++ should-i-backend/config/application.rb
@@ -19,6 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module ShouldIBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -32,6 +33,8 @@ module ShouldIBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
