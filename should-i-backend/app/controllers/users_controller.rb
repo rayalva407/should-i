93fbaf3ef975b8c18661dc612d35103bf9e21040
rayalva407 <rayalva407@gmail.com>
2020-11-26 13:32:19 -0700

    sets up users_controller.rb

--- should-i-backend/app/controllers/users_controller.rb
+++ should-i-backend/app/controllers/users_controller.rb
@@ -12,4 +12,39 @@ class UsersController < ApplicationController
       }
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['no users found']
+      }
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      renderjson: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
