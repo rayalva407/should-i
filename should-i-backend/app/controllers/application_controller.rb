65c39ebce7e9f37405c20fd8542faaed3d64e453
rayalva407 <rayalva407@gmail.com>
2020-11-26 14:29:10 -0700

    adds more helper methods

--- should-i-backend/app/controllers/application_controller.rb
+++ should-i-backend/app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  helper_method :login!
+  helper_method :login!, :logged_in?, :current_user
 
   def login!
     session[:user_id] = @user.id
   end
-  
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
