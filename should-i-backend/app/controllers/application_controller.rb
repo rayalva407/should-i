e4edbc3de5e4d17f9eb01e0fb85dcbcce0bf3311
rayalva407 <rayalva407@gmail.com>
2020-11-26 14:35:12 -0700

    more helpersgit add .

--- should-i-backend/app/controllers/application_controller.rb
+++ should-i-backend/app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authorized_user?
+    @user == current_user
+  end
+
+  def logout!
+    session.clear
+  end
+
+  def set_user
+    @user = User.find_by(id: session[:user_id])
+  end
+
 end
