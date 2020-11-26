d269f0978ba20f325adc170364352e8d5d194b76
rayalva407 <rayalva407@gmail.com>
2020-11-26 14:47:23 -0700

    adds sessions routes

--- should-i-backend/config/routes.rb
+++ should-i-backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :show, :index]
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
 end
